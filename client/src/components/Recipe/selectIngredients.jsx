597478ca2edbe0bece4f024e1393a37f24b980e5
Sascha Rose <sascha.rose@gmail.com>
2024-11-21 14:54:28 +0100

    backup

--- client/src/components/Recipe/selectIngredients.jsx
+++ client/src/components/Recipe/selectIngredients.jsx
@@ -2,17 +2,13 @@ import Select from "react-select";
 import { useState, useEffect } from "react";
 import { useFetch } from "../../hooks/fetch.jsx";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export function ZutatenForm() {
   const { fetchData } = useFetch();
-
   const [ingredientsData, setIngredientsData] = useState([]);
-
   const [ingredientsArr, setIngredientsArr] = useState([{ ingredient: "" }]);
-  const [ingredientObj, setingredientObj] = useState({
-    ingredient: "",
-  });
-
-  let API_URL = import.meta.env.VITE_API_URL;
+  const [ingredientObj, setIngredientObj] = useState({ ingredient: "" });
 
   // *** Daten für Kategorien und Zutaten laden ***
   useEffect(() => {
@@ -24,13 +20,16 @@ export function ZutatenForm() {
   }, []);
   // ---------------------------
 
+  useEffect(() => {
+    console.debug("useEffect after ingredientObj has changed");
+  }, [ingredientObj]);
+
   const handleAddIngredient = () => {
     setIngredientsArr([
       ...ingredientsArr,
       { ingredient: ingredientObj.ingredient },
     ]);
-
-    setingredientObj({ ingredient: "" });
+    setIngredientObj({ ingredient: "" });
   };
 
   const handleRemoveIngredient = (array, index) => {
@@ -47,11 +46,13 @@ export function ZutatenForm() {
   return (
     <form onSubmit={handleSubmit}>
       <h1>Zutaten hinzufügen</h1>
+      <div>{JSON.stringify(ingredientsArr)}</div>
       {ingredientsArr.map((ingredient, index) => (
         <div key={index} className="ingredient-row">
           <Select
             className="basic-single"
-            value={ingredient.ingredient}
+            // defaultInputValue={JSON.stringify(ingredient)}
+            // value={ingredient.ingredient}
             classNamePrefix="select"
             isClearable="true"
             isSearchable="true"
@@ -66,9 +67,9 @@ export function ZutatenForm() {
                 : []
             }
             onChange={(e) => {
-              console.log("obj", ingredientObj, "target value", e);
-              console.log("Value:", e.value);
-              setingredientObj({
+              console.log("--> obj", ingredientObj, "target value", e);
+              console.log("--> Value:", e.value);
+              setIngredientObj({
                 ...ingredientObj,
                 ingredient: e ? e.value : "",
               });
