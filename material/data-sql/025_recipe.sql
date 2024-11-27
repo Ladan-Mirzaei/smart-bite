64fe5119cb700ae1b5b9643e06e12dbb40c6952e
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-11-27 12:20:16 +0100

    recipe-table

--- material/data-sql/025_recipe.sql
+++ material/data-sql/025_recipe.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE recipe (
-    id SERIAL PRIMARY KEY,
+     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES recipe_user(id),
-    title VARCHAR(255) NOT NULL, 
-    description TEXT, 
-    preparation_time INT, 
-    cooking_time INT, 
-    portions INT, 
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    preparation_time INT,
+    cooking_time INT,
+    portions INT,
     category_id INT REFERENCES recipe_categories(id), 
-    difficulty_level VARCHAR(100) CHECK (difficulty_level IN ('einfach', 'mittel', 'schwer')), 
-    instructions TEXT,
-    created_at DATE
+    difficulty_level VARCHAR(100) CHECK (difficulty_level IN ('einfach', 'mittel', 'schwer')),
+    created_at DATE,
+    image TEXT
 );
