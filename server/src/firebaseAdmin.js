c2f36b8b3be09f849cdd962cb0d76848f731044a
Sascha Rose <sascha.rose@gmail.com>
2024-11-27 10:57:05 +0100

    catch firebase error

--- server/src/firebaseAdmin.js
+++ server/src/firebaseAdmin.js
@@ -2,13 +2,17 @@ import admin from "firebase-admin";
 import { readFileSync } from "fs";
 
 // JSON-Datei lesen
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./service-account-key.json", import.meta.url))
-);
+try {
+  const serviceAccount = JSON.parse(
+    readFileSync(new URL("./service-account-key.json", import.meta.url))
+  );
 
-// Firebase Admin SDK initialisieren
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  // Firebase Admin SDK initialisieren
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} catch (error) {
+  console.error("Error: ", error);
+}
 
 export default admin;
