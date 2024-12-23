f7f61fa78ab3f899aac1ab422ddadddc30411f6b
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-12-23 09:36:45 +0100

    change recipedetails

--- server/src/firebaseGetAuth.js
+++ server/src/firebaseGetAuth.js
@@ -1,25 +1,4 @@
 //https://medium.com/@asatpathy314/implementing-authentication-middleware-with-firebase-and-express-f811754e815b
-// import admin from "./firebaseAdmin.js";
-
-// const firebaseGetAuth = async (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     next();
-//   }
-
-//   const idToken = authHeader.split("Bearer ")[1];
-
-//   try {
-//     const decodedToken = await admin.auth().verifyIdToken(idToken);
-//     req.user = decodedToken; // Füge den Benutzer zum Request-Objekt hinzu
-//     next();
-//   } catch (error) {
-//     next();
-//   }
-// };
-
-// export default firebaseGetAuth;
 
 import admin from "./firebaseAdmin.js";
 
